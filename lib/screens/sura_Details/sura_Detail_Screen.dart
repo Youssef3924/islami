ee504a33adae55a28695e675eb48150724a5132e
youssef <youssefashraf112293@gmail.com>
2026-01-22 15:37:21 +0200

    create sura details screen

---
+++ lib/screens/sura_Details/sura_Detail_Screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SuraDetailScreen extends StatelessWidget {
+  static const String routeName="SuraDetailsScreen";
+  const SuraDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
