db1db551cf74f7103f97ef6886aae8eefb48a54e
youssef <youssefashraf112293@gmail.com>
2026-01-23 12:53:57 +0200

    WIP-sura Details screen UI

--- lib/screens/sura_Details/sura_Detail_Screen.dart
+++ lib/screens/sura_Details/sura_Detail_Screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/colors.dart';
+import 'package:islami/core/styles.dart';
+import 'package:islami/models/sura_model.dart';
 
 class SuraDetailScreen extends StatelessWidget {
   static const String routeName="SuraDetailsScreen";
@@ -6,6 +9,52 @@ class SuraDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var model =ModalRoute.of(context)?.settings.arguments as SuraModel;
+    return Scaffold(
+      backgroundColor: AppColors.black,
+    appBar:AppBar(
+      backgroundColor: AppColors.black,
+      centerTitle:true ,
+      iconTheme: IconThemeData(
+        color: AppColors.primary
+      ),
+      title: Text(model.nameEN,
+      style: Styles.style1!
+      ,
+      ),
+    ) ,
+      body: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+          Image.asset("assets/images/Soura_Details.png",
+            fit: BoxFit.cover,
+          ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 35.0),
+                  child: Text(model.name,
+                  style: Styles.style1,),
+                ),
+                SizedBox(height: 40,),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) =>
+                        Center(
+                          child: Text("data",
+                            style: Styles.style1 ,
+                          ),
+                        ),
+                  ),
+                ),
+                SizedBox(height: 100,)
+              ],
+            ),
+
+        ],),
+      ),
+    );
   }
 }
