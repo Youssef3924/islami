74730b2bf53b4b589c5053c0e1df32bc9948fc6e
youssef <youssefashraf112293@gmail.com>
2026-01-25 02:57:42 +0200

    hadith tap ready

--- lib/screens/Home/tabs/hadith_tab.dart
+++ lib/screens/Home/tabs/hadith_tab.dart
@@ -1,8 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/models/hadith_model.dart';
 
-class hadith extends StatelessWidget {
-  const hadith({super.key});
+class hadith extends StatefulWidget {
+   hadith({super.key});
 
+  @override
+  State<hadith> createState() => _hadithState();
+}
+
+class _hadithState extends State<hadith> {
+   List<HadithModel> allAhadithData=[];
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadAllHadithFile();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -11,6 +26,74 @@ class hadith extends StatelessWidget {
               fit: BoxFit.fill,
               image: AssetImage("assets/images/hadith_pg.png"))
       ),
+      child:CarouselSlider(
+        options: CarouselOptions(height: double.infinity),
+        items: allAhadithData.map((model) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                  padding: EdgeInsets.only(top: 150),
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                    Image.asset("assets/images/hadith_card.png"),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: Column(
+                        children: [
+                          Text(model.title,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: model.content.length,
+                              itemBuilder:
+                              (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(18.0),
+                                  child: Text(
+                                    model.content[index],
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                );
+                              },
+
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],)
+              );
+            },
+          );
+        }).toList(),
+      )
+
     );
   }
+
+   Future<void> loadAllHadithFile () async
+   {
+     for(int i=1;i<=50;i++)
+     {
+       String hadithFile = await rootBundle.loadString(
+           "assets/files2/h$i.txt");
+       List<String> hadithLines = hadithFile.split("\n");
+       String title = hadithLines[0];
+       hadithLines.removeAt(0);
+       List<String> hadithContent = hadithLines;
+       allAhadithData.add(HadithModel(title, hadithContent));
+     }
+     setState(() {
+
+     });
+   }
 }
